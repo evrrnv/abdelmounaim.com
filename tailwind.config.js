ab81e291e7b5b7f5225dd1587892e949a00144d0
evrrnv <github.y9fqc@leave.email>
2022-06-25 13:33:07 +0100

    three cards section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
                 black: "#040404",
                 emperor: "#4F4F4F",
                 "electric-violet": "#834BFF",
-                "purple-heart": "#6238BF"
+                "purple-heart": "#6238BF",
+                "alto": "#CECECE",
+                concrete: "#F2F2F2"
             },
             backgroundImage: {
                 'whitetro': "linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%)"
