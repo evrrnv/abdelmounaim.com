d18e0c8854008e4b73626ca4d1353d4a2560a4b7
evrrnv <github.y9fqc@leave.email>
2022-07-01 20:55:56 +0100

    fix shadow in homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
     content: ["./public/**/*.html"],
     theme: {
@@ -10,13 +12,19 @@ module.exports = {
                 emperor: "#4F4F4F",
                 "electric-violet": "#834BFF",
                 "purple-heart": "#6238BF",
-                "alto": "#CECECE",
-                concrete: "#F2F2F2"
+                alto: "#CECECE",
+                concrete: "#F2F2F2",
             },
             backgroundImage: {
-                'whitetro': "linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%)"
-            }
+                whitetro: "linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%)",
+            },
         },
     },
-    plugins: [require("tailwindcss"), require("autoprefixer")],
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+        plugin(function ({ addVariant }) {
+            addVariant("open", ".open &");
+        }),
+    ],
 };
